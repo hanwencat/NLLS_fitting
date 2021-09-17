ea0a1440f4ad1c999a34097466cc8926124ee129
hanwencat <hanwencat@gmail.com>
2021-09-17 00:13:50 -0400

    able to run without bug but results are not good

--- script_Fitting.m
+++ script_Fitting.m
@@ -1,24 +1,30 @@
-fname_img = 'imgCoilOffsetCorr.mat';
-fname_F = 'F_VSFFunction.mat';
-fname_b = 'B0_midResults.mat';
+% fname_img = 'imgCoilOffsetCorr.mat';
+% fname_F = 'F_VSFFunction.mat';
+% fname_b = 'B0_midResults.mat';
 
-used_TE = 16;
+used_te = 16;
 
-s = load(fname_img);
-img = s.imgAllEcho_comb;
+% s = load(fname_img);
+% img = s.imgAllEcho_comb;
+img = iField;
+%img = iField(:,:,:,2:2:end);
 
-s = load(fname_F);
-Fx = s.Fx;
-Fy = s.Fy;
-Fz = s.Fz;
 
-load(fname_b, 'b'); % Field map in unit of T
-gammabar = 42577482.5; % in unit of Hz/T
-b_hz = b * gammabar; % Field map in unit of Hz
+% s = load(fname_F);
+% Fx = s.Fx;
+% Fy = s.Fy;
+% Fz = s.Fz;
+Fz = 1;
 
-clear s;
+% load(fname_b, 'b'); % Field map in unit of T
+% gammabar = 42577482.5; % in unit of Hz/T
+% b_hz = b * gammabar; % Field map in unit of Hz
+b_hz = tfs*1000/(2*pi);
 
-mask = niftiread('img3_mag_comb_brain_mask.nii.gz');
+% clear s;
+
+% mask = niftiread('img3_mag_comb_brain_mask.nii.gz');
+mask = Mask;
 
 if size(img(:,:,:,1)) ~= size(mask)
     error('Image and Mask should have the same size.');
@@ -28,21 +34,25 @@ xpt = size(mask,1);
 ypt = size(mask,2);
 zpt = size(mask,3);
 
-TE = 2.5 : 1.5 : 49.0; % In unit of ms
+te = TE*1000; % In unit of ms
+% te = TE(1:2:end)*1000;
 
-if used_TE > length(TE)
-    error('TE used for fitting cannot larger than total TE number.');
+if used_te > length(te)
+    error('te used for fitting cannot larger than total te number.');
 end
 
-t_used = TE(1:used_TE);
+t_used = te(1:used_te);
 
-tot_slice = 40;
-% tot_slice = 1:zpt;
+% tot_slice = 40;
+tot_slice = 1:zpt;
 
 imgVSFCorr = abs(img)./ Fz;
 imgVSFCorr(isnan(imgVSFCorr)) = 0;
 imgVSFCorr(isinf(imgVSFCorr)) = 0;
-imgVSFCorr = imgVSFCorr .* exp(1j*angle(img));
+%imgVSFCorr = imgVSFCorr .* exp(1j*angle(img));
+imgVSFCorr = imgVSFCorr .* exp(1j*iFreq_all_echo_corr);
+%imgVSFCorr = imgVSFCorr .* exp(1j*iFreq_all_echo_corr(:,:,:,1:2:end));
+
 % clear img;
 
 % objfcn = @(p,t) (p(1) * exp(-1/p(4)*t - 1j*2*pi*p(7)*t) ...
@@ -61,9 +71,9 @@ opts = optimoptions(@lsqnonlin, 'Algorithm', 'trust-region-reflective',...
 %% Start Fitting
 disp('Started Fitting.');
 tic;
-parpool(4);
-s = tot_slice;
-% for s = tot_slice
+parpool(8);
+% s = tot_slice(40);
+for s = 1 : zpt
     parfor x = 1 : xpt
         for y = 1 : ypt
             
@@ -75,11 +85,11 @@ s = tot_slice;
             decay = decay / max(abs(decay));
             decay(isnan(decay)) = 0;
             decay(isinf(decay)) = 0;
-            decay = double(decay(1:used_TE));
+            decay = double(decay(1:used_te));
             decay = abs(decay).*exp(1j*unwrap(angle(decay)));
                                   
             p0 =...
-                [0.1*abs(decay(1)); 0.6*abs(decay(1)); 0.4*abs(decay(1)); ...
+                [0.1*abs(decay(1)); 0.6*abs(decay(1)); 0.3*abs(decay(1)); ...
                 10; 64; 48;...
                 b_hz(x,y,s); b_hz(x,y,s); b_hz(x,y,s); ...
                 angle(decay(1))];
@@ -91,7 +101,7 @@ s = tot_slice;
                 -pi];
             
             p_upper = ...
-                [0.1*abs(decay(1)); 0.6*abs(decay(1)); 0.4*abs(decay(1)); ...
+                [2*abs(decay(1)); 2*abs(decay(1)); 2*abs(decay(1)); ...
                 24; 1000; 1000; ...
                 b_hz(x,y,s) + 75; b_hz(x,y,s) + 25; b_hz(x,y,s) + 25; ...
                 pi];
@@ -105,11 +115,11 @@ s = tot_slice;
             objfcn = @(p)objfun_3pool_cplx_lsqnonlin(p,t_used,decay);
             [p_est,resnorm] = lsqnonlin(objfcn, p0, p_lower, p_upper, opts);
 
-            fitted_param(x, y, :) = reshape([p_est; resnorm], [1,1,11]);
+            fitted_param(x, y, s, :) = reshape([p_est; resnorm], [1,1,1,11]);
             
         end
     end
-% end
+end
 delete(gcp('nocreate'));
 
 toc;
